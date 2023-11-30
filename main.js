a2159ab6f5365473ba09a1d9afb721a45160d565
Ksenia <MY_NAME@example.com>
2023-11-30 19:15:19 +0300

    fix header

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ import './src/components/grid/grid.js';
 import './src/components/forms/forms.js';
 import './src/components/button/button.js';
 import './src/components/app-header/app-header.js';
-import './src/components/main-menu/main-menu.js'
+import './src/components/main-menu/main-menu.js';
+import './src/components/app-logo/app-logo.js';
 
 import './src/global/utils.scss';
